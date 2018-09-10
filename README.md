18a570dfbfa3ece25250f77cc7b1c3be3ed5755c
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2018-09-10 15:08:41 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Robert Ellwanger - ECE 309 - 8/30/2018
+## Table of Contents
+- [Homework_2](#Homework_2)
+
+### Homework_2
+- Add StringList.cpp
---
+++ lib/StringList.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class List {
+private:
+	class ListNode {
+	private:
+		string item; 
+		ListNode *next;
+	public:
+		ListNode(string a) 
+			{ item = a; next=NULL; }
+		ListNode* getNext() { return next; }
+		void setNext(ListNode *n) { next = n; }
+		string getItem() { return item; }
+	};
+	ListNode *head;
+	ListNode *tail;
+public:
+	List()
+	{
+		head = NULL;
+		tail = NULL;
+	}
+	void push_back(string a)
+	{
+		ListNode *node = new ListNode(a);
+		if (head==NULL) {  
+			head = node;
+			tail = node;
+		} else {
+			tail->setNext(node); 
+			tail = node;         
+		}
+	}  
+	bool remove_front(string &a)
+	{
+		if (!empty()) 
+		{
+			string copy = head->getItem(); 
+			ListNode *tmp = head->getNext();
+			delete head; 
+			head = tmp;  
+			if (tmp==NULL) 
+				tail = NULL;
+			return true;
+		}
+		return false; 
+	}   
+	bool empty()
+	{
+		return head==NULL;
+	}
+	string get(int n)
+	{
+		int i = 0;
+		ListNode* currNode = head;
+		
+		while (currNode != NULL)
+		{
+			if (i == n)
+				return currNode->getItem();		
+			i++;
+			currNode = currNode->getNext();
+		}
+		
+		return "";
+	}
+	int length()
+	{
+		int i = 0;
+		ListNode* currNode = head;
+		
+		while (currNode != NULL)
+		{
+			++i;
+			currNode = currNode->getNext();
+		}
+		return i;
+	}
+	~List()
+	{
+		string t;
+		while(!empty()) 
+			remove_front(t);   
+	}
+};
