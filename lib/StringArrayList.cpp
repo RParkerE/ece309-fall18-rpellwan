28c9321983665a60f56904f66a61df9df3647dd9
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2018-09-11 10:58:29 -0400

    Add files via upload

---
+++ lib/StringArrayList.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class StringArrayList{
+private:
+	string* arrList;
+public:
+	StringArrayList(int size=1)
+	{
+		arrList = new string[size];
+	}
+	void push_back(string s)
+	{
+		int i;
+		for(i = 0; i < sizeof(arrList); i++)
+		{
+			if(arrList[i] ==  "")
+				arrList[i] = s;
+		}
+		string* tmpArrList = new string[sizeof(arrList) + 1];
+		tmpArrList = arrList;
+		tmpArrList[-1] = s;
+		delete[] arrList;
+		arrList = new string[sizeof(tmpArrList)];
+		arrList = tmpArrList;
+		delete[] tmpArrList;
+	}
+	string get(int n)
+	{
+		if(n > 0 && n < sizeof(arrList))
+			return arrList[n];
+		else
+			return "";
+	}
+	int length()
+	{
+		return sizeof(arrList);
+	}
+	string remove_front(){
+		int i;
+		string tmp;
+		tmp = arrList[0];
+		for(i = 0; i < sizeof(arrList - 1); i++)
+		{
+			arrList[i] = arrList[i + 1];
+		}
+		return tmp;
+	}
+	~StringArrayList()
+	{
+		delete[] arrList;
+	}
+};
