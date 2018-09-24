da1e3d3fd9440b992e705b68861c9a096e984edf
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2018-09-24 15:40:59 -0400

    fix unused variable

--- lib/PList.cpp
+++ lib/PList.cpp
@@ -57,7 +57,7 @@ public:
 			tail = node;         
 		}
 	}  
-	bool remove_front(Object &a)
+	Object remove_front(Object &a)
 	{
 		if (!empty()) 
 		{
@@ -67,7 +67,7 @@ public:
 			head = tmp;  
 			if (tmp==NULL) 
 				tail = NULL;
-			return true;
+			return copy;
 		}
 		return false; 
 	}   
