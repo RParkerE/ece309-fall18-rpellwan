85d18ec07694c32133376461a92fcdf1ca950712
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2018-10-11 18:13:17 -0400

    Add files via upload

---
+++ lib/IntegerSetArray.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class IntegerSet {
+protected:
+   int size;
+   int hash(int key) { return (key * 997) % size; }
+public:
+   IntegerSet(int htsize):size(htsize) {}  
+   virtual bool insert(int) = 0;
+   virtual bool search(int) const = 0;
+   virtual void remove(int) = 0;
+};
+
+class IntegerSetArray : public IntegerSet {
+private:
+	int numItems;
+	int size;
+	int* array;
+public:
+	IntegerSetArray(int htsize):IntegerSet(htsize)
+	{
+		array = new int[htsize];
+		for(int i = 0; i < htsize; i++)
+			array[i] = -1;
+		size = htsize;
+		numItems = 0;
+	}
+	~IntegerSetArray();
+	bool insert(int x);
+	bool search(int x) const;
+	void remove(int x);
+};
+
+bool IntegerSetArray::insert(int x)
+{
+	printf("SIZE = %d, NUM ITEMS = %d",size, numItems);
+    if(numItems+1 > size)
+		return false;
+    array[numItems] = x;
+    numItems += 1;
+	return true;
+}
+
+bool IntegerSetArray::search(int x) const
+
+{
+	int pos;
+	for(int i = 0; i < size; i++)
+	{
+		if(array[i] == x)
+		{
+			pos = i;
+			return true;
+		}
+	}
+	return false;
+}
+
+void IntegerSetArray::remove(int x)
+{
+	int* newArr = new int[size];
+	int count = 0; int current = 0;
+	for(int i = 0; i < size; i++)
+	{
+		if(array[i] == x)
+			count += 1;
+		
+		else
+			newArr[i - count] = array[i];
+	}
+	delete[] array;
+    array = newArr;
+	numItems -= count;
+}
+
+IntegerSetArray::~IntegerSetArray()
+{
+	delete[] array;
+}
