bc607f8178a5827d72af7b8944545eef6c00b7df
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2018-11-19 20:23:36 -0500

    Update InfInt_Test.cpp

--- tests/InfInt_Test.cpp
+++ tests/InfInt_Test.cpp
@@ -3,11 +3,14 @@
 #include "InfInt.h"
 
 int main() {
+  bool test;
   IntInf x = IntInf();
   IntInf y = IntInf(false, true, false, false, -1);
   IntInf z = IntInf();
   x = 5; x = x + 10; x = x * 2; x = x - 0;
-  x = x / 3; x > 11; x < 11; x == 10;
+  x = x / 3; 
+  test = x > 11; test = x < 11;  test = x == 10;
   z = y; z = z / x; z = z * x; z = z + x;
   z = z - x;
+  test = z < x;
 }
